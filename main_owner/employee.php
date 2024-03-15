290e2aa94d06b9c8ec0f57c51e26f3bf9120c90b
TasnimHoque <83399964+TasnimHoque@users.noreply.github.com>
2024-03-15 22:26:18 +0600

    employee.php

--- main_owner/employee.php
+++ main_owner/employee.php
@@ -1,4 +1,26 @@
-<?php include '../includes/header.php'; ?>
-<h1>Employee</h1>
-<!-- Plugin Content 1 -->
-<?php include '../includes/footer.php'; ?>
+<?php include '../../includes/header.php'; ?>
+<h1>Employees</h1>
+<p>Manage employees and their profiles here.</p>
+
+<div class="employee-management">
+    <h2>Employee List</h2>
+  
+    <div class="employee">
+        <h3>Employee Name 1</h3>
+        <p>Email: employee1@example.com</p>
+        <p>Position: Position Title</p>
+    
+        <button>Edit Profile</button>
+        <button>Delete Employee</button>
+    </div>
+    <div class="employee">
+        <h3>Employee Name 2</h3>
+        <p>Email: employee2@example.com</p>
+        <p>Position: Position Title</p>
+
+        <button>Edit Profile</button>
+        <button>Delete Employee</button>
+    </div>
+ 
+</div>
+<?php include '../../includes/footer.php'; ?>
