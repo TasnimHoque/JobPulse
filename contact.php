8c4cbac67fbe0f92396a5be4d58baefb922fbcc2
TasnimHoque <83399964+TasnimHoque@users.noreply.github.com>
2024-03-15 22:11:16 +0600

    contact.php

---
+++ contact.php
@@ -0,0 +1,4 @@
+<?php include 'includes/header.php'; ?>
+<h1>Contact Us</h1>
+<p>Have questions or feedback? Contact us using the form below or via email at info@jobpulse.com.</p>
+<?php include 'includes/footer.php'; ?>
